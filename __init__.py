0b27ed1e4534f52c249600e02f1c8dbe719b95d3
yousramahi11 <yousra.mahimoussa@icloud.com>
2025-12-03 09:51:20 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -12,7 +12,7 @@ def MaPremiereAPI():
                                                                                                                                        
 @app.route('/')
 def hello_world():
-  return render_template('hello.html') #COMM
+  return render_template('hello.html') #COMM2
 
 @app.route('/tawarano/')
 def meteo():
