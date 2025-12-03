c331cfc9c17fd2af8963458f21b6a499bdaa01f8
yousramahi11 <yousra.mahimoussa@icloud.com>
2025-12-03 10:01:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,15 +4,16 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
-app = Flask(__name__) 
+
+app = Flask(__name__)
+
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-                                                                                                                                       
+def contact():
+    return render_template("contact.html")
+
 @app.route('/')
 def hello_world():
-  return render_template('hello.html') #COMM2
+    return render_template('hello.html')  # COMM
 
 @app.route('/tawarano/')
 def meteo():
@@ -22,26 +23,9 @@ def meteo():
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Kelvin → °C
-        results.append({
-            'Jour': dt_value,
-            'temp': temp_day_value
-        })
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  # Kelvin -> °C
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
 
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
-
-@app.route("/histogramme/")
-def histogramme():
-    return render_template("histogramme.html")
-
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-
-
-  
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
